af4338b3eb70a72d50fe672c3fcd7072f0b434e4
Angela Basnet <angellabasnet@gmail.com>
2025-01-21 15:36:18 +0545

    Added images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import { MainNav } from "@/components/HomePage/Navbar/Main_Nav";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <MainNav />
-    </>
+      <div
+        className="bg-cover bg-center h-screen"
+        style={{ backgroundImage: "url('/assets/images/wholeView.jpg')" }}
+      >
+        <h1>Welcome to Our Website</h1>
+        <p>This is a page with a background image.</p>
+      </div>
+    </div>
   );
 }
