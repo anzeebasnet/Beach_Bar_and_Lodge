5a831cc0926922e527f6b5b456a405fa66cb58c3
Angela Basnet <angellabasnet@gmail.com>
2025-01-26 15:51:22 +0545

    updated booking widget

--- src/components/HomePage/Booking_Widget/BookingWidget.tsx
+++ src/components/HomePage/Booking_Widget/BookingWidget.tsx
@@ -128,7 +128,9 @@ export default function BookingWidget() {
         </Accordion>
         <div className="md:grid hidden gap-4 md:grid-cols-5 md:gap-6">
           <div className="mt-4">
-            <h2 className="text-xl font-semibold text-white ">Book Online</h2>
+            <h2 className="text-xl font-semibold text-white uppercase">
+              Book Online
+            </h2>
             <span className="text-sm text-yellow-500">
               Best price guaranteed!!
             </span>
@@ -143,7 +145,7 @@ export default function BookingWidget() {
                 <Button
                   variant="outline"
                   className={cn(
-                    "w-full justify-start bg-white text-left font-normal hover:bg-white",
+                    "w-full justify-start bg-white text-left font-normal hover:bg-white rounded",
                     !checkIn && "text-muted-foreground"
                   )}
                 >
@@ -172,7 +174,7 @@ export default function BookingWidget() {
                 <Button
                   variant="outline"
                   className={cn(
-                    "w-full justify-start bg-white text-left font-normal hover:bg-white",
+                    "w-full justify-start bg-white text-left font-normal hover:bg-white rounded",
                     !checkOut && "text-muted-foreground"
                   )}
                 >
@@ -203,7 +205,7 @@ export default function BookingWidget() {
           <div className="flex items-end">
             <Link href="/rooms">
               <Button
-                className="w-full bg-green-600 text-white hover:bg-green-700"
+                className="w-full bg-green-600 text-white hover:bg-green-700 rounded"
                 size="lg"
               >
                 FIND ROOM
