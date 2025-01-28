39f7d56d6d057069bcc514177ab0db64b55aa0fd
Angela Basnet <angellabasnet@gmail.com>
2025-01-28 10:47:04 +0545

    added text expansion functionality

--- src/components/HomePage/Blog_Section/BlogSection.tsx
+++ src/components/HomePage/Blog_Section/BlogSection.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Imperial_Script, Red_Hat_Display } from "next/font/google";
+"use client";
+import React, { useState } from "react";
+import { Imperial_Script, Montserrat, Red_Hat_Display } from "next/font/google";
 import blogs from "../../../lib/data/blogs.json";
 import Image from "next/image";
 import { BlogType } from "../../../../types/types";
@@ -14,15 +15,25 @@ const red_hat_display = Red_Hat_Display({
   subsets: ["latin"],
 });
 
+const montserrat = Montserrat({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
 const BlogSection = () => {
+  const [expanded, setExpanded] = useState<{ [key: number]: boolean }>({});
+
+  const toggleExpand = (id: number) => {
+    setExpanded((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
   return (
     <div>
-      <div className="flex flex-col gap-6 items-center justify-center sm:my-24 my-10 sm:px-8 px-4">
-        <h2
-          className={`${imperialScript.className} font-semibold sm:text-8xl text-6xl text-black`}
-        >
-          Blogs
-        </h2>
+      <div className="flex flex-col gap-6 items-center justify-center sm:my-24 my-10 sm:mt-10 mt-6 sm:px-8 px-4">
+        <h2 className={`${imperialScript.className} custom-h2`}>Blogs</h2>
         <div className="lg:w-[50vw] md:w-[60vw] w-full flex flex-col md:gap-10 sm:gap-6 gap-4">
           {blogs?.map((item: BlogType) => (
             <div
@@ -37,12 +48,22 @@ const BlogSection = () => {
                 className="rounded w-full"
               />
               <h2
-                className={`${red_hat_display.className} sm:text-4xl text-xl font-medium text-primary text-center`}
+                className={`${montserrat.className} sm:text-4xl text-xl font-medium text-primary text-center`}
               >
                 {item.title}
               </h2>
-              <p className="sm:text-base text-xs text-black text-justify sm:leading-7">
-                {item.desc}
+              <p
+                className={`${red_hat_display.className} sm:text-base text-sm text-black sm:leading-7`}
+              >
+                {expanded[item.id]
+                  ? item.desc
+                  : `${item.desc.substring(0, 185)}... `}
+                <button
+                  className="text-blue-500 font-medium hover:underline"
+                  onClick={() => toggleExpand(item.id)}
+                >
+                  {expanded[item.id] ? "View Less" : "View More"}
+                </button>
               </p>
             </div>
           ))}
