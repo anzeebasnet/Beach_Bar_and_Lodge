7c5f5d96bd32cc1ab3d0a455ca104c53a3f11184
Angela Basnet <angellabasnet@gmail.com>
2025-01-31 11:46:46 +0545

    created booking page

--- src/app/(pages)/booking/page.tsx
+++ src/app/(pages)/booking/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import AvailableRoomCard from "@/components/Cards/AvailableRoomCard";
+import AvailableRoom from "@/components/(Pages)/Booking/AvailableRooms";
 import BookingForm from "@/components/Forms/BookingForm";
+import { useAppSelector } from "@/lib/store/hooks";
+import { RootState } from "@/lib/store/store";
+import { BookingDetails } from "@/types/types";
 import {
   Imperial_Script,
   Montserrat,
@@ -26,26 +29,32 @@ const roboto = Roboto_Condensed({
 
 const page = () => {
   const [viewMode, setViewMode] = useState<"form" | "cards">("form");
+  const [bookingDetails, setBookingDetails] = useState<BookingDetails | null>(
+    null
+  );
+  const bookingView = useAppSelector(
+    (state: RootState) => state.bookingView.currentBookingView
+  );
 
-  const handleSubmitBooking = () => {
+  const handleSubmitBooking = (values: BookingDetails) => {
+    setBookingDetails(values);
     setViewMode("cards");
   };
 
+  const handleUpdateBookingDetails = (updatedDetails: BookingDetails) => {
+    setBookingDetails(updatedDetails);
+    console.log(updatedDetails);
+  };
+
   return (
-    <main
-      className={`${roboto.className} container mx-auto px-4 py-8 max-w-7xl`}
-    >
-      <div className="flex flex-col items-center sm:mb-12 mb-6">
+    <main className={`${roboto.className} container mx-auto p-5 max-w-7xl`}>
+      <div className="flex flex-col items-center mb-6">
         <h2 className={`${imperialScript.className} custom-h2`}>Book Now</h2>
         <p className={`${montserrat.className} custom-text`}>
           Turn your dream getaway into reality!
         </p>
       </div>
-      {viewMode === "form" ? (
-        <BookingForm handleSubmitBooking={handleSubmitBooking} />
-      ) : (
-        <AvailableRoomCard />
-      )}
+      {bookingView === "form" ? <BookingForm /> : <AvailableRoom />}
     </main>
   );
 };
